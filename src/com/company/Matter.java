bc1cca6ef1889e5e47346fa4bf052e5fa5430d77
Sebastian Krein <sebastiankrein@SebastiansMacBookPro.local>
2017-09-28 15:37:19 +0200

    Calculated vector X and Y

--- src/com/company/Matter.java
+++ src/com/company/Matter.java
@@ -8,9 +8,9 @@ public class Matter extends Node {
 
     private float _mass;
 
-    private float _posX;
+    private float _posX, _posY;
 
-    private float _posY;
+    private float _vectorX, _vectorY;
 
     public float getCenterOfMassPosX() {
         return this._posX;
@@ -20,16 +20,60 @@ public class Matter extends Node {
         return this._posY;
     }
 
+    private float getPositionWithVectorX() {
+        return _posX + _vectorX;
+    }
+
+    private float getPositionWithVectorY() {
+        return _posY + _vectorY;
+    }
+
     public void calculateForce(NodeInterface body) {
 
         if (!this.equals(body)) {
 
-            double force, distance;
-            distance = this.getDistanceTo(body);
+            double distanceSquared, acceleration, force, radians;
 
-            // First, calculate the force between two masses
-            // F = (G * m1 * m2) / r^2
-            force = (G * this._mass * body.getMass()) / Math.pow(distance, 2);
+            distanceSquared = Math.pow(this.getDistanceTo(body), 2);
+
+            // Firstly, calculate the force which takes effect on this particle
+            // F = (G * m1 * m2) / d^2
+            force = (G * this._mass * body.getMass()) / distanceSquared;
+
+            // Secondly, the force will lead to vector changes on the current force vectors
+            // The amount of change is determined by looking to the actual force compared with the position of both particles
+            // relatively to each other PLUS the actual vector of the current particle.
+
+            // This is the acceleration which acts on this particle = m/s2
+            acceleration = force / this._mass;
+
+            //acceleration *= 1000000000;
+            //acceleration *= 500000;
+
+
+            double x1, x2, y1, y2;
+            x1 = getCenterOfMassPosX();
+            y1 = getCenterOfMassPosY();
+            x2 = body.getCenterOfMassPosX();
+            y2 = body.getCenterOfMassPosY();
+
+            // Radian between two masses
+            radians = Math.atan((y2 - y1) / (x2 - x1));
+
+            radians = Math.atan2((y2 - y1), (x2 - x1));
+
+            // The distance my particle would move as hypotenuse in meters, alias "magnitude of the vector"
+            double vectorMagnitude = (acceleration / 2) * Math.pow(SECONDS_PASSED_BY, 2);
+
+            double sinAlpha = Math.sin(radians);
+            double cosAlpha = Math.cos(radians);
+            // sin(alpha) = oppositeSide / hypotenuse
+            double vectorY = vectorMagnitude * sinAlpha;
+            // cos(alpha) = adjacentSide / hypotenuse
+            double vectorX = vectorMagnitude * cosAlpha;
+
+            this._vectorX += vectorX;
+            this._vectorY += vectorY;
         }
     }
 
