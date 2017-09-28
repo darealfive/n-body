d3f40796857c6fc9b37d206adf607a1f08f90a08
Sebastian Krein <sebastiankrein@SebastiansMacBookPro.local>
2017-09-28 10:39:50 +0200

    Bugfix at wrong calculation of distance between two coordinates

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -2,11 +2,19 @@ package com.company;
 
 public abstract class Node implements NodeInterface {
 
-    public double getDistanceTo(NodeInterface body) {
+    /**
+     * Gets the absolute distance between two coordinates.
+     * <p>
+     * Gratulation to Stephan Bothur! He tells me how to get absolute distance between two points :)
+     *
+     * @param body
+     * @return double
+     */
+    protected double getDistanceTo(NodeInterface body) {
 
         double distanceX, distanceY;
-        distanceX = Math.abs(this.getCenterOfMassPosX() - body.getCenterOfMassPosX());
-        distanceY = Math.abs(this.getCenterOfMassPosY() - body.getCenterOfMassPosY());
+        distanceX = Math.abs(this.getCenterOfMassPosX() + (body.getCenterOfMassPosX() * -1));
+        distanceY = Math.abs(this.getCenterOfMassPosY() + (body.getCenterOfMassPosY() * -1));
 
         return Math.hypot(distanceX, distanceY);
     }
