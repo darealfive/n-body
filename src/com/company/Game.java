8705ca15f5f6deb330798f54353d8ef5619106fb
Sebastian Krein <sebastiankrein@SebastiansMacBookPro.local>
2017-10-07 12:00:08 +0200

    Added comments

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -50,73 +50,48 @@ public class Game extends BasicGame {
 
         int fac = 10;
         //Nicht normale Veränderungen
-        universe.space.addMass(new DebugMatter(0.5, -0.1, 250, 285, mass*fac));
+        /*universe.space.addMass(new DebugMatter(0.5, -0.1, 250, 285, mass*fac));
         universe.space.addMass(new DebugMatter(-0.5, -0.1, 250, 225, mass*fac));
+        universe.space.addMass(new DebugMatter(0, 0, 300, 500, 2));*/
 
+
+
+        /* //Zwei gleiche Massen haben stets die gleichen Vectoren, auch wenn ein vector einer Masse 0 ist, so ist der andere ebenfalls 0
+        universe.space.addMass(new DebugMatter(0.5, 0, 250, 285, mass*fac));
+        universe.space.addMass(new DebugMatter(-0.5, 0, 250, 225, mass*fac));
         universe.space.addMass(new DebugMatter(0, 0, 300, 500, 2));
+        */
 
 
         //universe.space.addMass(new Matter(0, 0, 0, 10, mass));
         //universe.space.addMass(new Mass(0, 10, mass));
 
-        /*
+
+        //Wenn gleiche 4 Massen umeinander rotieren, so ist die Summe der X/Y Vectoren immer gleich.
+        //Dabei ist der X Vector 0 wenn die Masse an den horizontalen Extrempunkten des virtuellen Kreises positioniert ist.
+        //Dabei ist der Y Vector 0 wenn die Masse an den vertikalen Extrempunkten des virtuellen Kreises positioniert ist.
         universe.space.addMass(new DebugMatter(-0.35, 0, 100, 120, mass));
         universe.space.addMass(new DebugMatter(0.35, 0, 100, 80, mass));
         universe.space.addMass(new DebugMatter(0, -0.35, 80, 100, mass));
         universe.space.addMass(new DebugMatter(0, 0.35, 120, 100, mass));
+
+
+        /*//Hier herscht gleichgewicht
+        universe.space.addMass(new Mass(150, 250, mass));
+        universe.space.addMass(new Mass(150, 350, mass));
+        universe.space.addMass(new DebugMatter(200, 300, mass));
+        universe.space.addMass(new Mass(250, 250, mass));
+        universe.space.addMass(new Mass(250, 350, mass));
         */
 
 
-        //universe.space.addMass(new DebugMatter(0, 0, 10, 10, mass*5));
-        //universe.space.addMass(new DebugMatter(0, 0, 200, 10, mass));
-        //universe.space.addMass(new Matter(0, 0, 500, 500, mass));
-
-
-        //universe.space.addMass(new Mass(300, 400, mass));
-        //universe.space.addMass(new Mass(300, 200, mass));
-        //universe.space.addMass(new Mass(200, 300, mass));
-        //universe.space.addMass(new Mass(400, 300, mass));
-
-
-        /*universe.space.addMass(new DebugMatter(0,-0.1,300, 400, mass));
+        /*
+        universe.space.addMass(new DebugMatter(0,-0.1,300, 400, mass));
         universe.space.addMass(new DebugMatter(0,0.1,300, 200, mass));
         universe.space.addMass(new DebugMatter(0,0.1,200, 300, mass));
         universe.space.addMass(new DebugMatter(0,-0.1,400, 300, mass));
-        //TODO test this matter
-        universe.space.addMass(new DebugMatter(0, 0, 300, 300, 2));*/
-
-
-        //universe.space.addMass(new DebugMatter(0.1, 0, 200, 560, 6000));
-
-        /*
-        universe.space.bodies.addElement(new Matter(200, 450, 6000));
-        universe.space.bodies.addElement(new Matter(50, 300, 6000));
-        universe.space.bodies.addElement(new Matter( 200, 310, 999999999));
-        universe.space.bodies.addElement(new Matter(350, 300, 6000));
-        universe.space.bodies.addElement(new Matter(200, 150, 6000));
-*/
-
-        //universe.space.bodies.addElement(new Matter(0,0, 140, 190, 200));
-
-        //universe.space.bodies.addElement(new Matter(64, 64, 3));
-        //universe.space.bodies.addElement(new Matter(66, 66, 3));
-
-        //universe.space.bodies.addElement(new Matter(1, 1, 3));
-        // universe.space.bodies.addElement(new Matter(500, 500, 3));
-        // universe.space.bodies.addElement(new Matter(490, 490, 3));
-
-        /*universe.space.bodies.addElement(new Matter(100, 300, 1));
-        universe.space.bodies.addElement(new Matter(100, 100, 3));
-        universe.space.bodies.addElement(new Matter(100, 110, 3));
-        universe.space.bodies.addElement(new Matter(100, 120, 3));*/
-
-        /*universe.space.bodies.addElement(new Matter(0, 0, 1));
-        universe.space.bodies.addElement(new Matter(100, 100, 1));
-        universe.space.bodies.addElement(new Matter(300, 200, 3));
-        universe.space.bodies.addElement(new Matter(300, 220, 3));
-        universe.space.bodies.addElement(new Matter(200, 180, 3));
-        universe.space.bodies.addElement(new Matter(100, 180, 3));
-        universe.space.bodies.addElement(new Matter(200, 150, 3));*/
+        universe.space.addMass(new DebugMatter(0, 0, 300, 300, 2));
+        */
 
         /*for (int i = 0; i <= nBodies; i++) {
 
