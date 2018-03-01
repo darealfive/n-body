90519b8ca0c3c21480def8343add06c6987273b7
Sebastian Krein <sebastiankrein@SebastiansMacBookPro.local>
2018-03-01 13:32:22 +0100

    Fixed "contains" which does not check if a circle is completely within a square

--- src/com/company/Square.java
+++ src/com/company/Square.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
@@ -9,10 +10,16 @@ public class Square extends Rectangle {
     }
 
     public boolean contains(Shape other) {
+
+        if (other instanceof Circle) {
+
+            return getMinX() <= other.getMinX() && getMinY() <= other.getMinY() && getMaxX() >= other.getMaxX() && getMaxY() >= other.getMaxY();
+        }
+
         if (!other.intersects(this)) {
             return false;
         } else {
-            for(int i = 0; i < other.getPointCount(); ++i) {
+            for (int i = 0; i < other.getPointCount(); ++i) {
                 float[] pt = other.getPoint(i);
                 if (!this.contains(pt[0], pt[1])) {
                     return false;
